d00ce573c2dc1b3d51c5f5e96e8f73380e6187e8
Robinbucad <robin.bucad6@gmail.com>
2022-03-15 17:17:54 +0100

    .

--- back/src/app.js
+++ back/src/app.js
@@ -1,24 +1,52 @@
 import express from "express"
-//import router from './users/users.router.js'
+import router from './users/users.router.js'
 import cors from 'cors'
-//import routerAuth from './auth/auth.route.js'
+import http from 'http'
+import {Server} from 'socket.io'
 
-import conversationRoute from './routes/conversation.js'
-import messageRoute from './routes/message.js'
+import usersRouter from './users/users.router.js';
 
 
 const app = express()
-const port = 4000
+const port = 3001
+const server = http.createServer(app)
+
+const io = new Server(server, {
+    cors:{ // Llamo donde voy a abrir la aplicaion para que no salte cords
+        origin:"http://localhost:3000",
+        methods:["GET", "POSTS"],
+    }
+})
+
+io.on('connection',(socket) => {// connection es un evento de socket
+    console.log(`User connected`)
+
+    // socket.on("send_message", (data) => {
+    //    // socket.to(data.room).emit('receive_message',data)
+    //     console.log(data)
+    //   });
+
+    io.emit("hello", 'hello desde back')
+    socket.on("send_message", data => {
+        console.log(data)
+        io.emit("receive_message",data)
+        console.log(data.room)
+    })
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected', socket.id)
+    })
+}) 
+
 
 app.use(express.json())
 app.use(express.urlencoded({extended:'utf-8'}))
-app.use(cors())
 
 
-//app.use('/users', router)
-//app.use('/auth', routerAuth)
-app.use('/conversation', conversationRoute)
-app.use('message',messageRoute)
+
+app.use('/users', router)
 
 
-app.listen(port,() => console.log(`Se ha iniciado en el puerto: ${port}`))
+
+
+server.listen(port,() => console.log(`Se ha iniciado en el puerto: ${port}`))
